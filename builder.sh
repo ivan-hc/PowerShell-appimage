cbd928bad97385d8dd9473d37135fced80a8a3b0
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-19 02:51:34 +0200

    Update builder.sh

--- builder.sh
+++ builder.sh
@@ -3,7 +3,7 @@
 APP=powershell
 mkdir tmp
 cd ./tmp
-wget -q "$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')" -O appimagetool
+wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 chmod a+x ./appimagetool
 
 URL=$(curl -Ls https://api.github.com/repos/PowerShell/PowerShell/releases/latest | grep -w -v i386 | grep -w -v i686 | grep -w -v aarch64 | grep -w -v arm64 | grep -w -v armv7l | grep browser_download_url | grep -i "linux-x64.tar.gz" | cut -d '"' -f 4 | head -1)
@@ -38,6 +38,6 @@ export UNION_PRELOAD="${HERE}"
 exec "${HERE}"/pwsh "$@"
 EOF
 chmod a+x ./$APP.AppDir/AppRun
-ARCH=x86_64 VERSION=$(./appimagetool -v | grep -o '[[:digit:]]*') ./appimagetool -s ./$APP.AppDir
+ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./$APP.AppDir
 cd ..
 mv ./tmp/*.AppImage ./PowerShell-$VERSION-x86_64.AppImage
